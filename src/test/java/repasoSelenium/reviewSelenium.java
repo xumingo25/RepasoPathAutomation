33a93f4c826184da1a0a4a5a394a8c0285585a10
Domingo Saavedra <domingo.saavedra@tsoftglobal.com>
2023-03-10 15:08:52 -0300

    Se actualiza proyecto V1.1. Review Manejo DOM del sitio. uso de Xpath y estrategias de busqueda y practicar ARCAICAS para manipular elementos web de un sitio :D

--- src/test/java/repasoSelenium/reviewSelenium.java
+++ src/test/java/repasoSelenium/reviewSelenium.java
@@ -1,10 +1,12 @@
 package repasoSelenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class reviewSelenium {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
         //Configurar una propiedad para enlazar el browser al driver
 
         String rutaProyecto = System.getProperty("user.dir");
@@ -23,11 +25,24 @@ public class reviewSelenium {
 
         driver.get("https://open.spotify.com/?");
 
-        try {
-            Thread.sleep(5000);
-        }catch(Exception e){
-            System.out.println("Ocurrio un problema");
-        }
+        Thread.sleep(5000);
+
+        //Manipular un elemento web
+        //Formas
+
+        //#1 (Olvidenla despues)
+        driver.findElement(By.xpath("//button[contains(text(),'Registrarte')]")).click();
+
+        Thread.sleep(5000);
+
+        //Forma #2 (tampoco hacerlo asi)
+        By locatorTxtCorreo = By.id("email");
+
+        WebElement txtCorreo = driver.findElement(locatorTxtCorreo);
+
+        txtCorreo.click();
+
+        txtCorreo.sendKeys("pathAutomation@gmail.com");
 
         driver.close();
 
